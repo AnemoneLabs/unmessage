fa685efdbfa592470bf3d7e8acec47b79abdef54
Felipe Dau <dau@riseup.net>
2017-05-19 21:58:29 +0000

    Add a `Regex` class to the utils

--- unmessage/utils.py
+++ unmessage/utils.py
@@ -1,4 +1,27 @@
+import re
 from collections import namedtuple
+
+import attr
 
 
 Address = namedtuple('Address', 'host port')
+
+
+@attr.s
+class Regex(object):
+    pattern = attr.ib(attr.validators.instance_of(str))
+
+    def match(self, string, flags=0, match_end=True):
+        end = '$' if match_end else ''
+        return re.match(self.pattern + end, string, flags)
+
+    def search(self, string, flags=0):
+        return re.search(self.pattern, string, flags)
+
+
+Regex.peer_name = Regex(r'[a-zA-Z0-9_-]+')
+Regex.onion_domain = Regex(r'[a-z2-7]{16}\.onion')
+Regex.address_port = Regex(r'\d+')
+Regex.peer_identity = Regex(r'{}@{}:{}'.format(Regex.peer_name.pattern,
+                                               Regex.onion_domain.pattern,
+                                               Regex.address_port.pattern))
