7293bc809ad308f2cff7ae252014ce681a29bba2
Felipe Dau <dau@riseup.net>
2017-05-23 19:43:38 +0000

    Add a hypothesis test for `build_request_packet`

--- tests/test_packets.py
+++ tests/test_packets.py
@@ -11,8 +11,10 @@ from pyaxo import b2a
 
 CORRECT_LEN_IV = random(packets.IV_LEN)
 CORRECT_LEN_HASH = random(packets.HASH_LEN)
+CORRECT_LEN_KEY = random(packets.KEY_LEN)
 CORRECT_LEN_ENC_KEY = random(packets.ENC_KEY_LEN)
 CORRECT_LEN_PAYLOAD = random(1)
+CORRECT_LEN_HANDSHAKE_PACKET = random(1)
 
 
 def join_encode_data(lines):
@@ -89,3 +91,39 @@ def test_build_reply_packet(iv,
     else:
         with pytest.raises(errors.MalformedPacketError):
             packets.build_reply_packet(data)
+
+
+@given(
+    binary(),
+    binary(),
+    binary(),
+    binary(),
+    binary(),
+)
+@example(
+    CORRECT_LEN_IV,
+    CORRECT_LEN_HASH,
+    CORRECT_LEN_HASH,
+    CORRECT_LEN_KEY,
+    CORRECT_LEN_HANDSHAKE_PACKET,
+)
+def test_build_request_packet(iv,
+                              iv_hash,
+                              handshake_packet_hash,
+                              request_key,
+                              handshake_packet):
+    data = join_encode_data([iv,
+                             iv_hash,
+                             handshake_packet_hash,
+                             request_key,
+                             handshake_packet])
+    if (len(iv) == packets.IV_LEN and
+            len(iv_hash) == packets.HASH_LEN and
+            len(handshake_packet_hash) == packets.HASH_LEN and
+            len(request_key) == packets.KEY_LEN and
+            len(handshake_packet)):
+        assert isinstance(packets.build_request_packet(data),
+                          packets.RequestPacket)
+    else:
+        with pytest.raises(errors.MalformedPacketError):
+            packets.build_request_packet(data)
