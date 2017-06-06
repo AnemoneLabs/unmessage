64d63bab18c3a348a3fdf4f8e2d6573764a17baa
Felipe Dau <dau@riseup.net>
2017-06-06 02:00:25 +0000

    Improve key validation helper functions

--- unmessage/utils.py
+++ unmessage/utils.py
@@ -3,6 +3,8 @@ import re
 import attr
 from nacl.public import PublicKey
 
+from . import errors
+
 
 @attr.s
 class Address(object):
@@ -35,5 +37,32 @@ def is_valid_identity(value):
             Regex.peer_identity.match(value) is not None)
 
 
-def is_valid_pub_key(value):
+def is_valid_curve25519_key(value):
     return isinstance(value, bytes) and len(value) == PublicKey.SIZE
+
+
+def is_valid_priv_key(value):
+    return is_valid_curve25519_key(value)
+
+
+def raise_invalid_priv_key(instance=None, attribute=None, value=None):
+    if not is_valid_priv_key(value):
+        raise errors.InvalidPrivateError()
+
+
+def is_valid_pub_key(value):
+    return is_valid_curve25519_key(value)
+
+
+def raise_invalid_pub_key(instance=None, attribute=None, value=None):
+    if not is_valid_pub_key(value):
+        raise errors.InvalidPublicKeyError()
+
+
+def is_valid_shared_key(value):
+    return is_valid_curve25519_key(value)
+
+
+def raise_invalid_shared_key(instance=None, attribute=None, value=None):
+    if not is_valid_shared_key(value):
+        raise errors.InvalidSharedKeyError()
