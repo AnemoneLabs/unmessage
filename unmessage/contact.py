f73d4d9e320a733da320af2a8631484d2a3aa582
Felipe Dau <dau@riseup.net>
2017-07-02 23:58:13 +0000

    Add validators to the `bool` attrs of `Contact`

--- unmessage/contact.py
+++ unmessage/contact.py
@@ -7,8 +7,10 @@ from .utils import Address, raise_invalid_identity, raise_invalid_pub_key
 class Contact(object):
     identity = attr.ib(validator=raise_invalid_identity)
     key = attr.ib(validator=raise_invalid_pub_key)
-    is_verified = attr.ib(default=False)
-    has_presence = attr.ib(default=False)
+    is_verified = attr.ib(validator=attr.validators.instance_of(bool),
+                          default=False)
+    has_presence = attr.ib(validator=attr.validators.instance_of(bool),
+                           default=False)
 
     @property
     def name(self):
