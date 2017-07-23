3af3044e6e7721e4b5e508d3df9651d890a0a881
Felipe Dau <dau@riseup.net>
2017-07-23 15:25:35 +0000

    Add test to start and stop the peer

---
+++ tests/test_peer.py
@@ -0,0 +1,11 @@
+import pytest
+
+from unmessage.peer import Peer
+
+
+@pytest.inlineCallbacks
+def test_start_stop(reactor):
+    peer = Peer('pytest', reactor)
+    notification = yield peer.start(local_mode=True)
+    assert str(notification) == 'Peer started'
+    yield peer.stop()
