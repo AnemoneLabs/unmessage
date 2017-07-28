398f1fc987d21749051df0a519e39b12e1706456
Felipe Dau <dau@riseup.net>
2017-07-28 23:25:25 +0000

    Test request without starting/stopping the peers

--- tests/test_conversation.py
+++ tests/test_conversation.py
@@ -4,11 +4,7 @@ from unmessage.contact import Contact
 from unmessage.peer import Peer
 
 
-@pytest.inlineCallbacks
 def test_conversation_request(peer_a, peer_b):
-    yield peer_a.start(local_mode=True, local_server_port=12887)
-    yield peer_b.start(local_mode=True, local_server_port=13887)
-
     contact_a = Contact(peer_a.identity, peer_a.identity_keys.pub)
     out_request = peer_b._create_request(contact_a)
     packet = out_request.packet
@@ -18,6 +14,3 @@ def test_conversation_request(peer_a, peer_b):
 
     assert contact_b.identity == peer_b.identity
     assert contact_b.key == peer_b.identity_keys.pub
-
-    yield peer_a.stop()
-    yield peer_b.stop()
