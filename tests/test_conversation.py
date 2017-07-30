ee93f92abc0d85701fbc7aad2594e8850b42a772
Felipe Dau <dau@riseup.net>
2017-07-30 00:00:13 +0000

    Test conversation establishment

--- tests/test_conversation.py
+++ tests/test_conversation.py
@@ -1,7 +1,10 @@
 import pytest
+from twisted.internet import defer
 
 from unmessage.contact import Contact
-from unmessage.peer import Peer
+from unmessage.peer import b2a, Conversation, Peer
+
+from .utils import attach
 
 
 def test_conversation_request(peer_a, peer_b):
@@ -14,3 +17,21 @@ def test_conversation_request(peer_a, peer_b):
 
     assert contact_b.identity == peer_b.identity
     assert contact_b.key == peer_b.identity_keys.pub
+
+
+@pytest.inlineCallbacks
+def test_establish_conversation(peer_a, peer_b, conn_a, conn_b, mocker):
+    attach(peer_a, peer_b, conn_a, conn_b, mocker)
+
+    yield peer_b.send_request(peer_a.identity, b2a(peer_a.identity_keys.pub))
+    yield peer_a.accept_request(peer_b.identity)
+
+    conv_a = peer_a._conversations[peer_b.name]
+    conv_b = peer_b._conversations[peer_a.name]
+
+    assert conv_a.contact.identity == peer_b.identity
+    assert conv_b.contact.identity == peer_a.identity
+    assert conv_a.contact.key == peer_b.identity_keys.pub
+    assert conv_b.contact.key == peer_a.identity_keys.pub
+    assert conv_a.axolotl.id_ == conv_b.axolotl.id_
+    assert conv_a.keys.key == conv_b.keys.key
