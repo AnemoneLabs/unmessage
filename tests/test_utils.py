b6a9cf31cc8e802a53442ae6af2121cb7a781ff9
Felipe Dau <dau@riseup.net>
2017-08-01 23:43:36 +0000

    Test logger fixture

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,3 +9,11 @@ def test_paths_base():
     paths = Paths(head, tail)
     assert paths.base == str(paths)
     assert paths.base == join(head, tail)
+
+
+def test_logger_fixture(log):
+    info = 'test_utils.py::test_logger_fixture'
+    log.info(info)
+    with open('/tmp/unmessage.log', 'r') as f:
+        last_log = f.readlines()[-1].strip()
+    assert last_log.endswith(info)
