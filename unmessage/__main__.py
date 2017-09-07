88003a0ec05600dd4a831693317eac6a4526d60f
Felipe Dau <dau@riseup.net>
2017-09-07 15:40:44 +0000

    Identify UIs by class names

--- unmessage/__main__.py
+++ unmessage/__main__.py
@@ -8,8 +8,8 @@ from .log import Logger
 from .peer import CONFIG
 
 
-UIS = {cli.__name__: cli,
-       gui.__name__: gui}
+UIS = {cli.Cli.__name__: cli,
+       gui.Gui.__name__: gui}
 
 
 log = Logger()
@@ -18,15 +18,15 @@ log = Logger()
 def main():
     log.debug('Launching unMessage from the `unmessage` entry point')
     try:
-        ui_module = CONFIG.get('unMessage', 'ui')
-        ui = UIS[ui_module]
+        ui_class = CONFIG.get('unMessage', 'ui')
+        ui = UIS[ui_class]
     except (ConfigParser.NoSectionError, KeyError):
         log.debug('Using the {gui} when a valid previous UI is not found',
-                  gui=gui.__name__)
+                  gui=gui.Gui.__name__)
         gui.main()
     else:
         log.debug('Using the {ui} based on a previous launch',
-                  ui=ui.__name__)
+                  ui=ui_class.__name__)
         name = CONFIG.get('unMessage', 'name')
         ui.main(name)
 
